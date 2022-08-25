53ab344d0938449289e34e1a3f214fb56fb582df
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 20:52:20 -0300

    Adicionando funçoes de soma e subtração

--- Calculadora.js
+++ Calculadora.js
@@ -1,2 +1,14 @@
 var numA = parseInt(prompt("Digite um numero: "))
 var numB = parseInt(prompt("Digite mais um numero: "))
+SomarNumeros();
+SubtracaodosNumeros();
+function SomarNumeros (){
+    var soma = numA + numB
+    console.log("A soma dos valores são: ", numA + numB )
+    return soma;
+}
+function SubtracaodosNumeros (){
+    var subtracao = numA - numB
+    console.log("A subtração dos valores são: ", subtracao)
+    return subtracao;    
+}
