751ddcf46896d4d013ed7fe98c35b324d460544a
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 21:03:58 -0300

    Adicionando funçoes de multiplicação e divisao , e colocando parametros nas funções

--- Calculadora.js
+++ Calculadora.js
@@ -1,14 +1,26 @@
 var numA = parseInt(prompt("Digite um numero: "))
 var numB = parseInt(prompt("Digite mais um numero: "))
-SomarNumeros();
-SubtracaodosNumeros();
-function SomarNumeros (){
+SomarNumeros(numA , numB);
+SubtracaodosNumeros(numA , numB);
+MultiplicarNumeros(numA , numB);
+DividirNumeros(numA , numB);
+function SomarNumeros (numA , numB){
     var soma = numA + numB
     console.log("A soma dos valores são: ", numA + numB )
     return soma;
 }
-function SubtracaodosNumeros (){
+function SubtracaodosNumeros (numA , numB){
     var subtracao = numA - numB
     console.log("A subtração dos valores são: ", subtracao)
     return subtracao;    
+}
+function MultiplicarNumeros (numA , numB){
+    var multiplicacao = numA * numB
+    console.log("A multiplicação dos valores é: ", multiplicacao)
+    return multiplicacao
+}
+function DividirNumeros (numA , numB){
+    var divisao = numA / numB
+    console.log("A Divisão dos valores é: ", divisao)
+    return divisao
 }
