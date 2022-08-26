8bb4732efed8e887b4c0ddab064d462a90407c3c
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 22:09:54 -0300

    Foi adicionado um array para salvar todos os resultados e foi adicionando tambeém uma função que exibe o historico.

--- Calculadora.js
+++ Calculadora.js
@@ -3,6 +3,8 @@ var opcao = 0;
 var desejaContinuar = 0;
 var numA = 0;
 var numB = 0;
+var resultadosDasOperacoes = []
+var contador = 0;
 while(continuar){
     desejaContinuar = prompt("Deseja continuar? Digite 1 para Iniciar / continuar e 2 para encerrar:")
     if (desejaContinuar == 1){
@@ -14,9 +16,8 @@ while(continuar){
     function ExecutarProgramaNovamente(){
     numA = parseInt(prompt("Digite um numero: "))
     numB = parseInt(prompt("Digite mais um numero: "))
-    opcao = prompt("Digite 1 para Adição 2 para Subtração 3 para Multiplicação 4 Divisão e 5 para encerrar o programa.")
     }
-    
+    opcao = prompt("Digite 1 para Adição 2 para Subtração 3 para Multiplicação 4 Divisão e 5 para encerrar o programa.")
     if(opcao == 1){
         SomarNumeros(numA , numB);
     }
@@ -27,7 +28,7 @@ while(continuar){
         MultiplicarNumeros(numA , numB);
     }
     else if (opcao == 4){
-        DividirNumeros(numA , numB);
+        DividirNumeros(numA , numB)
     }
     else{
         continuar = false
@@ -36,21 +37,43 @@ while(continuar){
     function SomarNumeros (numA , numB){
         var soma = numA + numB
         console.log("A soma dos valores são: ", numA + numB )
-        return soma;
+        resultadosDasOperacoes [contador] = soma
+        contador++
+        //console.log("O resultado das operações foi: ", resultadosDasOperacoes)
+        return soma; 
     }
     function SubtracaodosNumeros (numA , numB){
         var subtracao = numA - numB
         console.log("A subtração dos valores são: ", subtracao)
+        resultadosDasOperacoes [contador] = subtracao
+        contador++
+        //console.log("O resultado das operações foi: ", resultadosDasOperacoes)
         return subtracao;    
     }
     function MultiplicarNumeros (numA , numB){
         var multiplicacao = numA * numB
         console.log("A multiplicação dos valores é: ", multiplicacao)
+        resultadosDasOperacoes [contador] = multiplicacao
+        contador++
+        //console.log("O resultado das operações foi: ", resultadosDasOperacoes)
         return multiplicacao
     }
     function DividirNumeros (numA , numB){
         var divisao = numA / numB
         console.log("A Divisão dos valores é: ", divisao)
+        resultadosDasOperacoes [contador] = divisao 
+        contador++
+        //console.log("O resultado das operações foi: ", resultadosDasOperacoes)
         return divisao
     }
+    function MostrarResultados (){ 
+        for(var atual = 0; atual < contador -5; atual++){
+            for(var seguinte = 0; seguinte < atual; seguinte++){
+                resultadosDasOperacoes[contador]
+                contador++
+            }
+        }
+        console.log("O Historico dos resultados são esse: ",resultadosDasOperacoes)
+    }
+    MostrarResultados();
 }
