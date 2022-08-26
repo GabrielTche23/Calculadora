4425676b0d14b166c09f5446452aae2fb6ad7a68
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 23:04:30 -0300

    Alterações na função do array,coloquei de volta osconsole.logs para acompanhar os testes.

--- Calculadora.js
+++ Calculadora.js
@@ -5,67 +5,68 @@ var numA = 0;
 var numB = 0;
 var resultadosDasOperacoes = []
 var contador = 0;
-while(continuar){
+while (continuar) {
     desejaContinuar = prompt("Deseja continuar? Digite 1 para Iniciar / continuar e 2 para encerrar:")
-    if (desejaContinuar == 1){
+    if (desejaContinuar == 1) {
         ExecutarProgramaNovamente();
     }
-    else{
+    else {
         continuar = false
     }
-    function ExecutarProgramaNovamente(){
-    numA = parseInt(prompt("Digite um numero: "))
-    numB = parseInt(prompt("Digite mais um numero: "))
+    function ExecutarProgramaNovamente() {
+        numA = parseInt(prompt("Digite um numero: "))
+        numB = parseInt(prompt("Digite mais um numero: "))
     }
-    opcao = prompt("Digite 1 para Adição 2 para Subtração 3 para Multiplicação 4 Divisão e 5 para encerrar o programa.")
-    if(opcao == 1){
-        SomarNumeros(numA , numB);
+    opcao = prompt("Digite 1 para Adição 2 para Subtração 3 para Multiplicação 4 Divisão 5 para exibir o Historico e 6 para Encerrar o programa.")
+    if (opcao == 1) {
+        SomarNumeros(numA, numB);
     }
-    else if(opcao == 2){
-        SubtracaodosNumeros(numA , numB);
+    else if (opcao == 2) {
+        SubtracaodosNumeros(numA, numB);
     }
-    else if (opcao == 3){
-        MultiplicarNumeros(numA , numB);
+    else if (opcao == 3) {
+        MultiplicarNumeros(numA, numB);
     }
-    else if (opcao == 4){
-        DividirNumeros(numA , numB)
+    else if (opcao == 4) {
+        DividirNumeros(numA, numB)
     }
-    else{
-        continuar = false
+    else if(opcao == 5) {
+        MostrarResultados();
     }
-    
-    function SomarNumeros (numA , numB){
-        var soma = numA + numB
-        resultadosDasOperacoes [contador] = soma
-        contador++
-        return soma; 
+    else if(opcao = 6){
+        continuar = false;
     }
-    function SubtracaodosNumeros (numA , numB){
-        var subtracao = numA - numB
-        resultadosDasOperacoes [contador] = subtracao
-        contador++
-        return subtracao;    
+}
+function SomarNumeros(numA, numB) {
+    var soma = numA + numB
+    console.log("O resultado da soma foi: ", soma)
+    resultadosDasOperacoes[contador] = soma
+    contador++
+    return soma;
+}
+function SubtracaodosNumeros(numA, numB) {
+    var subtracao = numA - numB
+    console.log("O resultado da subtração foi: ",subtracao)
+    resultadosDasOperacoes[contador] = subtracao
+    contador++
+    return subtracao;
+}
+function MultiplicarNumeros(numA, numB) {
+    var multiplicacao = numA * numB
+    console.log("O resultado da multiplicação foi: ",multiplicacao)
+    resultadosDasOperacoes[contador] = multiplicacao
+    contador++
+    return multiplicacao
+}
+function DividirNumeros(numA, numB) {
+    var divisao = numA / numB
+    console.log("O resultado da divisão foi: ",divisao)
+    resultadosDasOperacoes[contador] = divisao
+    contador++
+    return divisao
+}
+function MostrarResultados() {
+    for (var index = 0; index < contador-1; index++) {
     }
-    function MultiplicarNumeros (numA , numB){
-        var multiplicacao = numA * numB
-        resultadosDasOperacoes [contador] = multiplicacao
-        contador++
-        return multiplicacao
-    }
-    function DividirNumeros (numA , numB){
-        var divisao = numA / numB
-        resultadosDasOperacoes [contador] = divisao 
-        contador++
-        return divisao
-    }
-    function MostrarResultados (){ 
-        for(var atual = 0; atual < contador -5; atual++){
-            for(var seguinte = 0; seguinte < atual; seguinte++){
-                resultadosDasOperacoes[contador]
-                contador++
-            }
-        }
-        console.log("O Historico dos resultados são esse: ",resultadosDasOperacoes)
-    }
-    MostrarResultados();
+    console.log("O Historico dos resultados são esse: ", resultadosDasOperacoes)
 }
