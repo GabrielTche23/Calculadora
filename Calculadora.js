9c3bd59664ba0e5286164e0dc3f8f1e46da4aeb9
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 21:18:09 -0300

    Foi adicionado a opção do usuario escolher qual operação na calculadora será executada, e tambem foi adicionado um laço de repetição para iniciar todo o programa e uma variavel para que o usario deseja ou nao continuar.

--- Calculadora.js
+++ Calculadora.js
@@ -1,26 +1,56 @@
-var numA = parseInt(prompt("Digite um numero: "))
-var numB = parseInt(prompt("Digite mais um numero: "))
-SomarNumeros(numA , numB);
-SubtracaodosNumeros(numA , numB);
-MultiplicarNumeros(numA , numB);
-DividirNumeros(numA , numB);
-function SomarNumeros (numA , numB){
-    var soma = numA + numB
-    console.log("A soma dos valores são: ", numA + numB )
-    return soma;
-}
-function SubtracaodosNumeros (numA , numB){
-    var subtracao = numA - numB
-    console.log("A subtração dos valores são: ", subtracao)
-    return subtracao;    
-}
-function MultiplicarNumeros (numA , numB){
-    var multiplicacao = numA * numB
-    console.log("A multiplicação dos valores é: ", multiplicacao)
-    return multiplicacao
-}
-function DividirNumeros (numA , numB){
-    var divisao = numA / numB
-    console.log("A Divisão dos valores é: ", divisao)
-    return divisao
+var continuar = true;
+var opcao = 0;
+var desejaContinuar = 0;
+var numA = 0;
+var numB = 0;
+while(continuar){
+    desejaContinuar = prompt("Deseja continuar? Digite 1 para Iniciar / continuar e 2 para encerrar:")
+    if (desejaContinuar == 1){
+        ExecutarProgramaNovamente();
+    }
+    else{
+        continuar = false
+    }
+    function ExecutarProgramaNovamente(){
+    numA = parseInt(prompt("Digite um numero: "))
+    numB = parseInt(prompt("Digite mais um numero: "))
+    opcao = prompt("Digite 1 para Adição 2 para Subtração 3 para Multiplicação 4 Divisão e 5 para encerrar o programa.")
+    }
+    
+    if(opcao == 1){
+        SomarNumeros(numA , numB);
+    }
+    else if(opcao == 2){
+        SubtracaodosNumeros(numA , numB);
+    }
+    else if (opcao == 3){
+        MultiplicarNumeros(numA , numB);
+    }
+    else if (opcao == 4){
+        DividirNumeros(numA , numB);
+    }
+    else{
+        continuar = false
+    }
+    
+    function SomarNumeros (numA , numB){
+        var soma = numA + numB
+        console.log("A soma dos valores são: ", numA + numB )
+        return soma;
+    }
+    function SubtracaodosNumeros (numA , numB){
+        var subtracao = numA - numB
+        console.log("A subtração dos valores são: ", subtracao)
+        return subtracao;    
+    }
+    function MultiplicarNumeros (numA , numB){
+        var multiplicacao = numA * numB
+        console.log("A multiplicação dos valores é: ", multiplicacao)
+        return multiplicacao
+    }
+    function DividirNumeros (numA , numB){
+        var divisao = numA / numB
+        console.log("A Divisão dos valores é: ", divisao)
+        return divisao
+    }
 }
